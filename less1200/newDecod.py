2161df07b47fbf253112f959fba7abf357fc0a49
Stanislav98V <74238483+Stanislav98V@users.noreply.github.com>
2021-02-13 10:05:16 +0300

    Add files via upload

--- less1200/newDecod.py
+++ less1200/newDecod.py
@@ -45,42 +45,62 @@ def state_prunning(tokens, best_tokens):
         best_tokens[state] = None
     return tokens
 
-def recognize(filename, features, fst_graph, thr_common, lm_scale):
+def recognize(filename, features, fst_graph, thr_common, lm_scale, wd_add):
     print("Recognizing file '{}', samples={}".format(filename,
                                                      features.nSamples))
     start_state = fst_graph.Start()
+    print('Start state = ', start_state)
     active_tokens = [Token(start_state), ] # Создаём токен
+    rt = GaussMixCompCache.GaussMixCompCache(AcoModels3.AcoModelSet, AcoModels3.AcoModel)
     next_tokens = []
     best_tokens = {}
     for state in range(fst_graph.NumStates()):
         best_tokens[state] = None
     for frame in range(features.nSamples): # Перебираем все векторы записи
+        current_frame_ftr = features.readvec()
+        GaussMixCompCache.GaussMixCompCache.setFrame(rt, ftr=current_frame_ftr)
         for token in active_tokens: # Перебираем активные токены
             if token.is_alive != False:
                 id1,id2 = fst_graph.GetArcIdRange(token.state)
                 for arc_id in range(id1,id2):
                     arc = fst_graph.GetArcById(arc_id)
-                    new_token = Token(arc.nextstate)
-                    new_token.dist +=  token.dist + arc.weight * lm_scale
-                    # if fst_graph.Final(arc.nextstate) != 'inf':
-                    #     new_token.dist += fst_graph.Final(arc.nextstate)
-                    new_token.sentence += token.sentence + wfst.invertSymbolTable(fst_graph.OutputSymbols)[arc.olabel]
-                    next_tokens.append(new_token)
+                    if wfst.invertSymbolTable(fst_graph.InputSymbols)[arc.ilabel] != '<eps>':
+                        new_token = Token(arc.nextstate)
+                        new_token.dist += token.dist + arc.weight * lm_scale
+                        new_token.dist += GaussMixCompCache.GaussMixCompCache.getDist(rt, arc.ilabel)
+                        new_token.sentence += token.sentence
+                        if type(fst_graph.Final(token.state)) == int or float:
+                            new_token.dist += fst_graph.Final(arc.nextstate) * lm_scale
+                        if wfst.invertSymbolTable(fst_graph.OutputSymbols)[arc.olabel] != '<eps>':
+                            if token.state != new_token.state:
+                                new_token.dist += wd_add
+                                new_token.sentence += wfst.invertSymbolTable(fst_graph.OutputSymbols)[arc.olabel] + ' '
+                        next_tokens.append(new_token)
+                    else:
+                        new_token = Token(arc.nextstate)
+                        new_token.dist += token.dist + arc.weight * lm_scale
+                        if type(fst_graph.Final(token.state)) == int or float:
+                            new_token.dist += fst_graph.Final(arc.nextstate) * lm_scale
+                        new_token.sentence += token.sentence
+                        next_tokens.append(new_token)
         next_tokens = state_prunning(next_tokens, best_tokens)
         next_tokens = beam_prunning(next_tokens, thr_common)
         active_tokens = next_tokens
         next_tokens = []
     print_tokens(active_tokens)
-    best_token_ol = active_tokens[np.argmin([i.dist for i in active_tokens])]
-    print("FINAAAAAAAAAL!!!!  ", best_token_ol.dist, best_token_ol.sentence)
-
+    if len(active_tokens) != 0:
+        best_token_ol = active_tokens[np.argmin([i.dist for i in active_tokens])]
+        print("FINAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAL!!!!  ", best_token_ol.dist, best_token_ol.sentence)
+    else:
+        print('No final token')
 if __name__ == "__main__":
-    InputSymbols = wfst.readSymbolTable("isyms.txt")
-    OutputSymbols = wfst.readSymbolTable("osyms.txt")
-    fst_graph = wfst.Wfst("danet.fst", InputSymbols,OutputSymbols)
-    records = "ark,t:test_MFCC.txtftr" # Считываем данные из файла запись
-    thr_common = 200
-    lm_scale = 0.9
+    InputSymbols = wfst.readSymbolTable("phones.txt")
+    OutputSymbols = wfst.readSymbolTable("phones.txt")
+    fst_graph = wfst.Wfst("daNetNoCyclic_4.fst", InputSymbols, OutputSymbols)
+    records = "ark,t:testNet.txtftr" # Считываем данные из файла запись
+    thr_common = 700
+    wd_add = 200
+    lm_scale = 100
     for filename, features in FtrFile.FtrDirectoryReader(records):
-        recognize(filename, features, fst_graph, thr_common, lm_scale) # Берём читалку и имя файла из записи и засовываем в recognize
+        recognize(filename, features, fst_graph, thr_common, lm_scale, wd_add) # Берём читалку и имя файла из записи и засовываем в recognize
     print("--- %s seconds ---" % (time.time() - start_time))
