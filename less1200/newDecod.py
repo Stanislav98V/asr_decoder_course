901c8f7973ca57934b04bf932f7341b8d7a1301e
Stanislav98V <74238483+Stanislav98V@users.noreply.github.com>
2021-02-15 10:04:31 +0300

    Add files via upload

--- less1200/newDecod.py
+++ less1200/newDecod.py
@@ -48,12 +48,12 @@ def state_prunning(tokens, best_tokens):
 def recognize(filename, features, fst_graph, thr_common, lm_scale, wd_add):
     print("Recognizing file '{}', samples={}".format(filename,
                                                      features.nSamples))
-    start_state = fst_graph.Start()
+    start_state = fst_graph.Start() # Находим стартовый токен
     print('Start state = ', start_state)
     active_tokens = [Token(start_state), ] # Создаём токен
     rt = GaussMixCompCache.GaussMixCompCache(AcoModels3.AcoModelSet, AcoModels3.AcoModel)
     next_tokens = []
-    best_tokens = {}
+    best_tokens = {} # Словарь для state_prunning, ключ это номер стейта, значение это лучший токен данного стейта
     for state in range(fst_graph.NumStates()):
         best_tokens[state] = None
     for frame in range(features.nSamples): # Перебираем все векторы записи
@@ -62,35 +62,33 @@ def recognize(filename, features, fst_graph, thr_common, lm_scale, wd_add):
         for token in active_tokens: # Перебираем активные токены
             if token.is_alive != False:
                 id1,id2 = fst_graph.GetArcIdRange(token.state)
-                for arc_id in range(id1,id2):
+                for arc_id in range(id1,id2): # Проходим все дуги стейта
                     arc = fst_graph.GetArcById(arc_id)
                     if wfst.invertSymbolTable(fst_graph.InputSymbols)[arc.ilabel] != '<eps>':
                         new_token = Token(arc.nextstate)
-                        new_token.dist += token.dist + arc.weight * lm_scale
+                        new_token.dist += token.dist + arc.weight * lm_scale # Прибавляем к дистанции предидущую дистанцию и вес дуги умноженный на коэф
                         new_token.dist += GaussMixCompCache.GaussMixCompCache.getDist(rt, arc.ilabel)
-                        new_token.sentence += token.sentence
-                        if type(fst_graph.Final(token.state)) == int or float:
+                        new_token.sentence += token.sentence # Прибавляем накопленные слова
+                        if type(fst_graph.Final(token.state)) == int or float: # Если стейт имеет вес, прибавляем его
                             new_token.dist += fst_graph.Final(arc.nextstate) * lm_scale
-                        if wfst.invertSymbolTable(fst_graph.OutputSymbols)[arc.olabel] != '<eps>':
-                            if token.state != new_token.state:
-                                new_token.dist += wd_add
-                                new_token.sentence += wfst.invertSymbolTable(fst_graph.OutputSymbols)[arc.olabel] + ' '
-                        next_tokens.append(new_token)
-                    else:
+                        if wfst.invertSymbolTable(fst_graph.OutputSymbols)[arc.olabel] != '<eps>': # Если на выходе дуги слово
+                            if token.state != new_token.state: # И предидущий стейт не равен текущему
+                                new_token.dist += wd_add # Прибавляем штраф за слово
+                                new_token.sentence += wfst.invertSymbolTable(fst_graph.OutputSymbols)[arc.olabel] + ' ' # Прибавляем слово
+                    else: # Если <eps> на входе дуги, проходим насквозь
                         new_token = Token(arc.nextstate)
-                        new_token.dist += token.dist + arc.weight * lm_scale
-                        if type(fst_graph.Final(token.state)) == int or float:
-                            new_token.dist += fst_graph.Final(arc.nextstate) * lm_scale
+                        new_token.dist += token.dist
                         new_token.sentence += token.sentence
-                        next_tokens.append(new_token)
+                    next_tokens.append(new_token)
         next_tokens = state_prunning(next_tokens, best_tokens)
-        next_tokens = beam_prunning(next_tokens, thr_common)
+        if len(next_tokens) > 0:
+            next_tokens = beam_prunning(next_tokens, thr_common)
         active_tokens = next_tokens
         next_tokens = []
     print_tokens(active_tokens)
     if len(active_tokens) != 0:
         best_token_ol = active_tokens[np.argmin([i.dist for i in active_tokens])]
-        print("FINAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAL!!!!  ", best_token_ol.dist, best_token_ol.sentence)
+        print("FINAL!!!!  ", best_token_ol.dist, best_token_ol.sentence)
     else:
         print('No final token')
 if __name__ == "__main__":
@@ -98,7 +96,7 @@ if __name__ == "__main__":
     OutputSymbols = wfst.readSymbolTable("phones.txt")
     fst_graph = wfst.Wfst("daNetNoCyclic_4.fst", InputSymbols, OutputSymbols)
     records = "ark,t:testNet.txtftr" # Считываем данные из файла запись
-    thr_common = 700
+    thr_common = 300
     wd_add = 200
     lm_scale = 100
     for filename, features in FtrFile.FtrDirectoryReader(records):
